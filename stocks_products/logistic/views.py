3b38bf6ec46c553cab3e2d6ac74a6c586386e510
Ivan Timofeev <timofeevis89@gmail.com>
2024-01-12 12:45:36 +0300

    homework

--- stocks_products/logistic/views.py
+++ stocks_products/logistic/views.py
@@ -1,16 +1,29 @@
+import django_filters
+from django_filters import FilterSet, NumberFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
 
-from logistic.models import Product, Stock
-from logistic.serializers import ProductSerializer, StockSerializer
+from .models import Product, Stock
+from .serializers import ProductSerializer, StockSerializer
+
+class ProductViewSetPagination(PageNumberPagination):
+    page_size = 3
+    page_query_param = 'page_size'
+    max_page_size = 5
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+    pagination_class = ProductViewSetPagination
+    filter_backends = [SearchFilter]
+    search_fields = ['description']
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['positions__product__id']
